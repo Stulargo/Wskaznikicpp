6e128e5732f034983fbb0397a7a57c7a30757afb
Stulargo <114095328+Stulargo@users.noreply.github.com>
2025-09-17 11:09:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,59 @@
 #include <iostream>
 
 using namespace std;
-class Tablica{
-int *tab = new int [10];
+
+class Tablica {
+    int *tab = new int[10];
+
 public:
-
-void wypelnienie(){
-
-for(int i=0;i<10;i++){
-        cin >> tab[i];
-    }
-}
-
-void wypisanie(){
-
-for(int i=0;i<10;i++){
-        cout << tab[i] << "";
-    }
-}
-
-private:
-int szukajacaMax(){
-
-    int maks = tab[0];
-    for(int i =0;i<10;i++){
-        if(maks<tab[i]){
-            maks=tab[i];
+    void wypelnienie() {
+        for (int i = 0; i < 10; i++) {
+            cin >> tab[i];
         }
     }
-    return maks;
 
-}
+    void wypisanie() {
+        for (int i = 0; i < 10; i++) {
+            cout << tab[i] << " ";
+        }
+        cout << endl;
+    }
+
+private:
+    int szukajacaMax() {
+        int maks = tab[0];
+        for (int i = 1; i < 10; i++) {
+            if (maks < tab[i]) {
+                maks = tab[i];
+            }
+        }
+        return maks;
+    }
 
 public:
-void sortowanie
-int maks;
-int temp;
-for(int i =0;i<10;i++){
+    void sortowanie() {
+        int maks;
+        int temp;
 
-    maks = this->szukajacaMax(i);
-    temp=tab[i];
-    tab[i]=maks;
-    maks=tep;
-}
+        for (int i = 0; i < 10; i++) {
+            maks = szukajacaMax();
+            temp = tab[i];
+            tab[i] = maks;
+            maks = temp;
+        }
+    }
 };
 
-int main()
-{
+int main() {
     cout << "Podaj elementy do tablicy: " << endl;
     Tablica tab;
     tab.wypelnienie();
+    cout << "Tablica przed sortowaniem: ";
     tab.wypisanie();
-    cout << endl;
-    //cout << tab.szukajacaMax();
+
+    tab.sortowanie();
+
+    cout << "Tablica po sortowaniu: ";
+    tab.wypisanie();
     return 0;
 }
